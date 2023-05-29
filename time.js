ad9264b04ebf5a6c0bc8e50fdac101c8a0ed1284
agony._dev <caiojose36415@gmail.com>
2023-05-29 14:50:23 -0300

    concluido
    adicionei comentários para me lembrar oq faz cada código no CSS e no JS

---
+++ time.js
@@ -0,0 +1,30 @@
+const hora = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time(){
+  let data = new Date()
+  let hr = data.getHours()
+  let min = data.getMinutes()
+  let sec = data.getSeconds() 
+
+  if(hr < 10 ){
+    hr = '0' + hr  
+}
+  if(min < 10){
+    min = '0' + min
+}
+  if(sec < 10){
+    sec = '0' + sec
+}
+  hora.textContent = hr
+  minutos.textContent = min
+  segundos.textContent = sec
+
+})
+
+/*
+
+O setInterval() serve para executar uma função ou instrução várias vezes em um determinado intervalo de tempo. A sua sintaxe é: setInterval(funcao, tempo); Isto significa que a função "funcao" será executada assim cada vez que se atingir o determinado tempo.
+
+*/
